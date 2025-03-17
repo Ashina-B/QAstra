b73a1e2aefec91f62bb505dcbc6dc48b0e9e7269
Ashina Barasa <ashina.barasa@griffinglobaltech.com>
2025-03-17 13:38:51 +0300

    fixed the issue with running lint

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+const { ESLint } = require("eslint");
+
+module.exports = [
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: require("@typescript-eslint/parser"),
+      parserOptions: {
+        project: "./tsconfig.json",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "error",
+    },
+  },
+];
