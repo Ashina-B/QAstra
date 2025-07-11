d5fa52450dfad7dec0d1f488526c9599e2eb1c95
ashina.barasa1 <ashina.barasa1@accesscfa.com>
2025-07-11 18:23:55 +0300

    Added child components to home

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,9 +3,16 @@ import { RegistrationComponent } from './users/registration/registration.compone
 import { LoginComponent } from './users/login/login.component';
 import { HomeComponent } from './home/home.component';
 import { AccountActivationComponent } from './users/account-activation/account-activation.component';
+import { ProjectComponent } from './project/project.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
+    {path: '', component: HomeComponent, 
+        children:[
+            {path: "project", component: ProjectComponent},
+            {path: "dashboard", component: DashboardComponent}
+        ]
+    },
     {path: 'register', component: RegistrationComponent},
     {path: 'login', component: LoginComponent},
     {path: 'activate-account', component:AccountActivationComponent}
